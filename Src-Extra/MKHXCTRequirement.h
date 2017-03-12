4162271168e4c7961b36a2f5a9cf410ae320d0c0
Maxim Khatskevich <maxim@khatskevi.ch>
2017-03-13 00:41:36 +0100

    Added base files

---
+++ Src-Extra/MKHXCTRequirement.h
@@ -0,0 +1,19 @@
+//
+//  MKHXCTRequirement.h
+//  MKHXCTRequirement
+//
+//  Created by Maxim Khatskevich on 3/13/17.
+//  Copyright © 2017 Maxim Khatskevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKHXCTRequirement.
+FOUNDATION_EXPORT double MKHXCTRequirementVersionNumber;
+
+//! Project version string for MKHXCTRequirement.
+FOUNDATION_EXPORT const unsigned char MKHXCTRequirementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKHXCTRequirement/PublicHeader.h>
+
+
